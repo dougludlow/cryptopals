11a85696037b7e025965cae0cd13420625b1ee83
Douglas Ludlow <doug.ludlow@royaljay.net>
2015-06-22 21:45:23 -0600

    Add solution to first challenge.

---
+++ sets/1/challenges/1.py
@@ -0,0 +1,14 @@
+import base64
+
+hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+solution = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+value = bytearray.fromhex(hex).decode()
+encoded = base64.b64encode(value)
+
+print value
+print solution
+print encoded
+print encoded == solution
+
+
