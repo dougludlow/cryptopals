1d844a6ac602b08fe3c0df39a08be198e4679099
Douglas Ludlow <doug.ludlow@royaljay.net>
2015-06-22 22:10:22 -0600

    Update docstrings.

--- sets/1/challenges/1.py
+++ sets/1/challenges/1.py
@@ -1,8 +1,10 @@
+"""Convert hex to base64."""
+
 import cryptopals
 
-hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 solution = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-result = cryptopals.hex_to_base64(hex)
+result = cryptopals.hex_to_base64(hex_string)
 
 print solution
 print result
--- sets/1/challenges/cryptopals.py
+++ sets/1/challenges/cryptopals.py
@@ -1,7 +1,7 @@
-"Reusable cryptopals module."
+"""Reusable cryptopals module."""
 
 import base64
 
 def hex_to_base64(string):
-    "Converts a hex string to base64."
+    """Converts a hex string to base64."""
     return base64.b64encode(bytearray.fromhex(string).decode())
