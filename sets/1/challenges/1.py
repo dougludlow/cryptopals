d0b1d2739e44ed8bea2f64303ab3b801cd45d37c
Douglas Ludlow <doug.ludlow@royaljay.net>
2015-06-22 22:05:46 -0600

    Move methods to module.

--- sets/1/challenges/1.py
+++ sets/1/challenges/1.py
@@ -1,14 +1,9 @@
-import base64
+import cryptopals
 
 hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 solution = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+result = cryptopals.hex_to_base64(hex)
 
-value = bytearray.fromhex(hex).decode()
-encoded = base64.b64encode(value)
-
-print value
 print solution
-print encoded
-print encoded == solution
-
-
+print result
+print result == solution
---
+++ sets/1/challenges/cryptopals.py
@@ -0,0 +1,7 @@
+"Reusable cryptopals module."
+
+import base64
+
+def hex_to_base64(string):
+    "Converts a hex string to base64."
+    return base64.b64encode(bytearray.fromhex(string).decode())
