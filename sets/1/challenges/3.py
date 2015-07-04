db65aac64e4abb83e0013207f98e925c27f7d88d
Douglas Ludlow <doug.ludlow@royaljay.net>
2015-07-04 17:13:58 -0600

    Don't use hardcoded list of characters.

--- sets/1/challenges/3.py
+++ sets/1/challenges/3.py
@@ -4,14 +4,14 @@ import cryptopals
 import etaoin_shrdlu
 
 xored_hex_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}\\/;:"\'<>,.? '
 
 results = {}
 scores = {}
 
-for c in characters:
-    results[c] = cryptopals.single_char_xor(xored_hex_string, c)
-    scores[c] = etaoin_shrdlu.score(results[c])
+for c in range(0, 255):
+    char = chr(c)
+    results[char] = cryptopals.single_char_xor(xored_hex_string, char)
+    scores[char] = etaoin_shrdlu.score(results[char])
 
 # top_5 = sorted(scores.items(), key=lambda x:x[1], reverse=True)[:5]
 #
